7975c3dd6c842951fe617d32676b279eda8078c6
Piggysoft <piggysoft0@gmail.com>
2018-12-03 17:45:01 +1100

    refactor: remove unused imports

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -6,9 +6,7 @@ import { default as User, UserModel, AuthToken } from "../models/User";
 import { Request, Response, NextFunction } from "express";
 import { IVerifyOptions } from "passport-local";
 import { WriteError } from "mongodb";
-import request from "express-validator";
 import "../config/passport";
-
 
 /**
  * GET /login
