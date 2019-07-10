9bd0d175325e416e7f1f427db976fe6d24660930
Piggysoft <piggysoft0@gmail.com>
2019-07-10 22:59:20 +0000

    Bump lodash from 4.17.11 to 4.17.13

--- src/types/express-flash.d.ts
+++ src/types/express-flash.d.ts
@@ -1,13 +1,9 @@
+
 /// <reference types="express" />
 
-/**
- * This type definition augments existing definition
- * from @types/express-flash
- */
+// Add RequestValidation Interface on to Express's Request Interface.
 declare namespace Express {
-    export interface Request {
-        flash(event: string, message: any): any;
-    }
+    interface Request extends Flash {}
 }
 
 interface Flash {
